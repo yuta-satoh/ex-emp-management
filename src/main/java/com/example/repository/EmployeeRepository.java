a67e6431549e8cf91a0e2c1292e6bd5776f22bb5
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2024-02-09 14:10:01 +0900

    Employee show list

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -14,7 +14,8 @@ import java.util.List;
 
 @Repository
 public class EmployeeRepository {
-    @Autowired NamedParameterJdbcTemplate template;
+    @Autowired
+    NamedParameterJdbcTemplate template;
 
     private static final String EMPLOYEE_FINDALL_SQL = """
             SELECT
@@ -33,8 +34,8 @@ public class EmployeeRepository {
             FROM
                 employees
             """;
-        
-        private static final String EMPLOYEE_FINDBYID_SQL = """
+
+    private static final String EMPLOYEE_FINDBYID_SQL = """
             SELECT
                 id,
                 name,
@@ -53,8 +54,8 @@ public class EmployeeRepository {
             WHERE
                 id = :id
             """;
-    
-        private static final String EMPLOYEE_UPDATE_SQL = """
+
+    private static final String EMPLOYEE_UPDATE_SQL = """
             UPDATE employees
             SET
                 name = :name,
@@ -70,51 +71,61 @@ public class EmployeeRepository {
                 dependents_count = :dependentsCount
             WHERE id = :id
             """;
-    
 
-        private static RowMapper<Employee> EMPROYEE_ROM_MAPPER = new BeanPropertyRowMapper<>(Employee.class);
+    private static RowMapper<Employee> EMPROYEE_ROM_MAPPER = (rs, index) -> {
+        Employee employee = new Employee();
+        employee.setId(rs.getInt("id"));
+        employee.setName(rs.getString("name"));
+        employee.setImage(rs.getString("image"));
+        employee.setGender(rs.getString("gender"));
+        employee.setHireDate(rs.getString("hire_date"));
+        employee.setMailAddress(rs.getString("mail_address"));
+        employee.setZipCode(rs.getString("zip_code"));
+        employee.setAddress(rs.getString("address"));
+        employee.setTelephone(rs.getString("telephone"));
+        employee.setSalary(rs.getInt("salary"));
+        employee.setCharacteristics(rs.getString("characteristics"));
+        employee.setDependentsCount(rs.getInt("dependents_count"));
+        return employee;
+    };
 
-        /**
-         * 全件取得
-         * @return 全ての従業員
-         */
-        public List<Employee> findAll(){
-            List<Employee> employees = template.query(EMPLOYEE_FINDALL_SQL, EMPROYEE_ROM_MAPPER);
-            return employees;
-        }
+    /**
+     * 全件取得
+     * 
+     * @return 全ての従業員
+     */
+    public List<Employee> findAll() {
+        List<Employee> employees = template.query(EMPLOYEE_FINDALL_SQL, EMPROYEE_ROM_MAPPER);
+        return employees;
+    }
 
-        /**
-         * 従業員1件取得
-         * @param id
-         * @return 従業員データ1件
-         */
+    /**
+     * 従業員1件取得
+     * 
+     * @param id
+     * @return 従業員データ1件
+     */
 
-        public Employee load(Integer id){
-            SqlParameterSource params = new MapSqlParameterSource()
-                .addValue("id", id);
-            Employee employee = template.queryForObject(EMPLOYEE_FINDBYID_SQL, params, EMPROYEE_ROM_MAPPER);
-            return employee;
-        }
+    public Employee load(Integer id) {
+        SqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
+        Employee employee = template.queryForObject(EMPLOYEE_FINDBYID_SQL, params, EMPROYEE_ROM_MAPPER);
+        return employee;
+    }
 
-        /**
-         * 従業員情報
-         * @param employee
-         * @param id
-         */
-        public void update(Employee employee, Integer id) {
-            MapSqlParameterSource params = new MapSqlParameterSource()
-                    .addValue("id", id)
-                    .addValue("name", employee.getName())
-                    .addValue("image", employee.getImage())
-                    .addValue("gender", employee.getGender())
-                    .addValue("hireDate", employee.getHireDate())
-                    .addValue("mailAddress", employee.getMailAddress())
-                    .addValue("zipCode", employee.getZipCode())
-                    .addValue("address", employee.getAddress())
-                    .addValue("telephone", employee.getTelephone())
-                    .addValue("salary", employee.getSalary())
-                    .addValue("characteristics", employee.getCharacteristics())
-                    .addValue("dependentsCount", employee.getDependentsCount());
-            template.update(EMPLOYEE_UPDATE_SQL,params);
-        }
+    /**
+     * 従業員情報
+     * 
+     * @param employee
+     * @param id
+     */
+    public void update(Employee employee, Integer id) {
+        MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id)
+                .addValue("name", employee.getName()).addValue("image", employee.getImage())
+                .addValue("gender", employee.getGender()).addValue("hireDate", employee.getHireDate())
+                .addValue("mailAddress", employee.getMailAddress()).addValue("zipCode", employee.getZipCode())
+                .addValue("address", employee.getAddress()).addValue("telephone", employee.getTelephone())
+                .addValue("salary", employee.getSalary()).addValue("characteristics", employee.getCharacteristics())
+                .addValue("dependentsCount", employee.getDependentsCount());
+        template.update(EMPLOYEE_UPDATE_SQL, params);
+    }
 }
