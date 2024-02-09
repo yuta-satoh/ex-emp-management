80a0ad1a1852f17fa000f74407b6036c04296716
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2024-02-09 13:40:31 +0900

    UpdateEmployeeForm

---
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -0,0 +1,26 @@
+package com.example.form;
+
+public class UpdateEmployeeForm {
+	/** 従業員ID */
+	private String id;
+	/** 扶養人数 */
+	private String dependentsCount;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDependentsCount() {
+		return dependentsCount;
+	}
+	public void setDependentsCount(String dependentsCount) {
+		this.dependentsCount = dependentsCount;
+	}
+	@Override
+	public String toString() {
+		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+	}
+
+	
+}
