9a70fc3af42abad39b33508705373f80b595f0bc
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2024-02-09 14:46:55 +0900

    employee detail

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.domain.Employee;
+import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -29,5 +32,13 @@ public class EmployeeController {
 		model.addAttribute("employees", employees);
 		return "employee/list";
 	}
+
+	@GetMapping("/showDetail")
+	public String showDetail(String id, Model model, UpdateEmployeeForm form) {
+		Employee employee = service.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		return "employee/detail";
+	}
+	
 	
 }
