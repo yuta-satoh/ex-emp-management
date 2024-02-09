d0eeaaac4bad99ac063d9da501ffd74ac765925d
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2024-02-09 09:22:35 +0900

    insert administrators

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,24 +1,45 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
 
 
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
 
+    @Autowired
+    private AdministratorService service;
+
     /**
      * 検索フォーム表示
      * @param form
-     * @return
+     * @return フォーム画面
      */
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
     }
-    
+
+    /** 
+     * データ挿入処理
+     * @param form
+     * @return ログイン画面
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator(); 
+        BeanUtils.copyProperties(form, administrator);
+        service.insert(administrator);
+        return "redirect:/"; 
+    }
+
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,9 +1,14 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -11,6 +16,13 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
     @Autowired
     private AdministratorRepository repository;
-    
+
+    /**
+     * 管理者登録
+     * @param name, mail_address, password
+     */
+    public void insert(Administrator administrator){
+        repository.insert(administrator);
+    }
     
 }
